99f674017dd320beb602cefd8bd0bb77f396a8cd
JoshuaAndle <68359516+JoshuaAndle@users.noreply.github.com>
2025-09-11 03:25:39 -0400

    Adding CIFAR to datasets.py
    Adding code for dataset and dataloader setup, with accompanying args in configs.py

--- Torch-Library/configs.py
+++ Torch-Library/configs.py
@@ -4,6 +4,9 @@ import argparse
 def args_parser():
     parser = argparse.ArgumentParser()
 
+    parser.add_argument('--dataset', type=str, default="CIFAR10", choices=["CIFAR10", '', ''], help="Name of dataset to train on")
+    parser.add_argument('--dataset_location', type=str, default="./data", help="Location to download datasets to")
+
     # parser.add_argument("--bool_arg", type=bool, default=True, help="")
     # parser.add_argument('--flag_arg', action='store_true', help="")
     # parser.add_argument('--int_arg', type=int, default=0, help="")
